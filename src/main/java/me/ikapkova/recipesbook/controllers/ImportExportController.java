a444f24c368caf8185d4eae0321f5cd123e49fac
Ирина Капкова <kapiral@rambler.ru>
2023-03-25 00:08:30 +0400

    6 commit - The ExportController determines the length and content of the file

--- src/main/java/me/ikapkova/recipesbook/controllers/ImportExportController.java
+++ src/main/java/me/ikapkova/recipesbook/controllers/ImportExportController.java
@@ -1,5 +1,4 @@
 package me.ikapkova.recipesbook.controllers;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import me.ikapkova.recipesbook.services.IngredientService;
@@ -7,14 +6,15 @@ import me.ikapkova.recipesbook.services.RecipeService;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
-
 @RestController
 public class ImportExportController {
     private final RecipeService recipeService;
@@ -29,14 +29,20 @@ public class ImportExportController {
     @GetMapping("/files/export/recipes")
     @Operation(summary = "Скачивание файла рецептов в JSON формате.")
     @ApiResponse(responseCode = "200", description = "Файл экспортирован.")
-    public ResponseEntity<Resource>downloadRecipes(){
+    public ResponseEntity<Resource> downloadRecipes() throws IOException {
         Resource recipes = recipeService.getRecipesFiles();
         ContentDisposition disposition = ContentDisposition.attachment()
                 .name("recipes.json")
                 .build();
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentDisposition(disposition);
-        return ResponseEntity.ok().headers(headers).body(recipes);
+        return ResponseEntity.ok()
+                .headers(headers)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"recipes.json\"")
+                .contentLength(recipes.contentLength())
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(recipes);
     }
     @PutMapping(value = "/files/import/recipes", consumes = MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "Импорт рецептов.", description = "Импорт файла рецептов в .json формате.")
@@ -45,12 +51,12 @@ public class ImportExportController {
         this.recipeService.importRecipes(file.getResource());
         return ResponseEntity.noContent().build();
     }
-//
+
     @PutMapping(value = "/files/import/ingredients", consumes = MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "Импорт ингридиентов.", description = "Импорт файла ингридиентов в .json формате.")
     @ApiResponse(responseCode = "200", description = "Файл загружен.")
     public ResponseEntity<?> importIngredients(@RequestParam MultipartFile file) {
         this.ingredientService.importIngrediens(file.getResource());
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok().build();
     }
 }
